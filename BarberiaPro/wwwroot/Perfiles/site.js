c835540d11056a7588d53beb929bff281b75164d
Chacho0 <hectorrafaeljaquezlanfrancon@gmail.com>
2025-03-07 12:05:59 -0400

    Reporte de pagos realizado!

---
+++ BarberiaPro/wwwroot/Perfiles/site.js
@@ -0,0 +1,8 @@
+﻿window.descargarArchivo = (base64, filename) => {
+    const link = document.createElement("a");
+    link.href = "data:application/pdf;base64," + base64;
+    link.download = filename;
+    document.body.appendChild(link);
+    link.click();
+    document.body.removeChild(link);
+};
